62653facecab2f4c93cda9361c0c752f9dc5d6e5
Aaviuxx <81174573+Aaviuxx@users.noreply.github.com>
2021-05-05 11:17:02 -0400

    Template
    Empty code, methods laid out, no body text

---
+++ Benford's Law Assignment.java
@@ -0,0 +1,34 @@
+/**
+ * Date: May 5, 2021
+ * Teacher: Mr. Ho
+ * Name: Ayeh Fartousi & Derek Xu
+ * Description: Part Two of Luhn Algorithmn Assignemnt
+ */
+
+// import Java packages here
+class BenfordLaw {
+    public static void main(String [] args) {
+        
+    }
+
+    /**
+     * @author Ayeh Fartousi
+     */
+    public static void loadSalesData (String [] args) {
+
+    }
+
+    /**
+     * @author Derek Xu
+     */
+    public static void analyzeSalesData (String [] args) {
+        
+    }
+
+    /**
+     * @author Derek Xu
+     */
+    public static void exportSalesData (String [] args) {
+        
+    }  
+}
